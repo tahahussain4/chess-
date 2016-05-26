cca1b243dfae0d528630b387a50f8f9f2d666817
LeNPaul <l.nguyen.paul@gmail.com>
2016-05-26 14:29:01 -0400

    Change movePiece function to update board position after a piece is moved

--- chessAI/main.cpp
+++ chessAI/main.cpp
@@ -3,13 +3,29 @@
 
 using namespace std;
 
-int movePiece(int currentPosition[2], int nextPosition[2])
+//Piece values refers to a value that designates a specific piece, and should not be confused with the relative value of the piece
+//Pawn: 1
+//Knight: 2
+//Bishop: 3
+//Rook: 4
+//Queen; 5
+//King: 6
+
+int movePiece(int pieceValue,int currentPosition[2], int nextPosition[2], int board[8][8])
 {
+    //These variables store how much the piece was moved in the row and column directions
     int rowMove;
     int columnMove;
 
+    //Calculate how far the piece was moved in the row and column direction
     rowMove = nextPosition[0] - currentPosition[0];
     columnMove = nextPosition[1] - currentPosition[1];
+
+    //Set the value of the square of the current piece position to zero
+    board[currentPosition[0]][currentPosition[1]] = 0;
+    //Set the value of the square of the next piece position to the piece value
+    board[currentPosition[0]][currentPosition[1]] = pieceValue;
+
 }
 
 int main()
@@ -27,6 +43,10 @@ int main()
                     {0,0,0,0,0,0,0,0},
                     {0,0,0,0,0,0,0,0}
                     };
+
+    //int current[2] = {5,5};
+    //int next[2] = {2,2};
+    //movePiece(3,current,next,board);
 
     return 0;
 }
