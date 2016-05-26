7d91626b5442bc6c70b2d571e5c8e0cabc991b83
LeNPaul <l.nguyen.paul@gmail.com>
2016-05-26 14:49:49 -0400

    Add print function that prints out the board array for visualization

--- chessAI/main.cpp
+++ chessAI/main.cpp
@@ -3,7 +3,7 @@
 
 using namespace std;
 
-//Piece values refers to a value that designates a specific piece, and should not be confused with the relative value of the piece
+//Piece ID refers to a value that designates a specific piece, and should not be confused with the relative value of the piece
 //Pawn: 1
 //Knight: 2
 //Bishop: 3
@@ -11,8 +11,20 @@ using namespace std;
 //Queen; 5
 //King: 6
 
-int movePiece(int pieceValue,int currentPosition[2], int nextPosition[2], int board[8][8])
+//Relative piece values determine which piece is worth more in order to make decisions
+//Pawn: 1
+//Knight: 3
+//Bishop: 3
+//Rook: 5
+//Queen; 9
+//King: over 9000
+
+int movePiece(int pieceID,int currentPosition[2], int nextPosition[2], int board[8][8])
 {
+
+    //This function moves a piece and updates the board array
+    //It takes in the current position of the piece and the final position of the piece, and updates the board array to reflect that move
+
     //These variables store how much the piece was moved in the row and column directions
     int rowMove;
     int columnMove;
@@ -24,7 +36,9 @@ int movePiece(int pieceValue,int currentPosition[2], int nextPosition[2], int bo
     //Set the value of the square of the current piece position to zero
     board[currentPosition[0]][currentPosition[1]] = 0;
     //Set the value of the square of the next piece position to the piece value
-    board[currentPosition[0]][currentPosition[1]] = pieceValue;
+    board[currentPosition[0]][currentPosition[1]] = pieceID;
+
+    return 0;
 
 }
 
@@ -44,9 +58,21 @@ int main()
                     {0,0,0,0,0,0,0,0}
                     };
 
-    //int current[2] = {5,5};
-    //int next[2] = {2,2};
-    //movePiece(3,current,next,board);
+    int current[2] = {5,5};
+    int next[2] = {2,2};
+    movePiece(3,current,next,board);
+
+    //Print out the board array for visualization
+    for(int i = 0; i < 8; i++)
+    {
+        for(int j = 0; j < 8; j++)
+        {
+            printf("%i",board[i][j]);
+        };
+
+        printf("\n");
+
+    };
 
     return 0;
 }
