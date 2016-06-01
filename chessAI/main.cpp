330d6a8d06c63a9714c6bbbcc85a33ef51c9782a
LeNPaul <l.nguyen.paul@gmail.com>
2016-06-01 12:42:29 -0400

    Replace files and fix bug in moveBishop function

--- chessAI/main.cpp
+++ chessAI/main.cpp
@@ -78,7 +78,7 @@ int main()
 
     int next[2] = {y - 1,x - 1};
 
-    moveRook(2,current,next,fullBoard);
+    moveBishop(2,current,next,fullBoard);
 
     printBoard(fullBoard);
 
--- chessAI/moveFunctions.h
+++ chessAI/moveFunctions.h
@@ -4,6 +4,7 @@
 //Function prototypes
 int movePiece(int pieceID,int currentPosition[2], int nextPosition[2], int board[8][8]);
 int moveRook(int pieceID,int currentPosition[2], int nextPosition[2], int board[8][8]);
+int moveBishop(int pieceID,int currentPosition[2], int nextPosition[2], int board[8][8]);
 int printBoard(int board[8][8]);
 
 #endif // MOVEFUNCTIONS_H_INCLUDED
